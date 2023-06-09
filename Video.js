9f2b08204819fc320daa0350e01dd8acb34ba35e
Mela0931 <mela3164@easv365.dk>
2023-06-09 15:39:08 +0200

    video side

---
+++ Video.js
@@ -0,0 +1,21 @@
+document.getElementsByTagName("ul")[0].addEventListener("click", function(evt) {
+
+    document.getElementById("menuToggler").checked = false;
+  
+    function handleTransitionEnd(transitionEvent) {
+      
+     
+      let contentDivs = document.querySelectorAll("main div");
+      for (var i = 0; i < contentDivs.length; i++) {
+        contentDivs[i].style.display = "none";
+      }
+  
+      let contentDiv = document.getElementById(contentId.substr(1));
+      contentDiv.style.display = "inherit";
+      
+     
+      sidebar.removeEventListener("transitionend", handleTransitionEnd);
+    }
+    
+    sidebar.addEventListener("transitionend", handleTransitionEnd);
+  });
